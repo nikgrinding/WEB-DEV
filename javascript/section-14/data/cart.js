86ea989239a1a3d6f1b961ac0ce042e63677f0b1
nikgrinding <nikgrinding@gmail.com>
2025-10-08 23:28:24 +0530

    08/10/2025

---
+++ javascript/section-14/data/cart.js
@@ -0,0 +1,49 @@
+export let cart = JSON.parse(localStorage.getItem('amazonCart')) || [
+	{
+		productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+		quantity: 2,
+	},
+	{
+		productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+		quantity: 1,
+	},
+];
+
+function saveToStorage() {
+	localStorage.setItem('amazonCart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
+	let matchingItem = '';
+	cart.forEach((cartItem) => {
+		if (cartItem.productId == productId) {
+			matchingItem = cartItem;
+		}
+	});
+
+	const stringQuantity = document.querySelector(
+		`.js-quantity-selector-${productId}`
+	).value;
+	const quantity = Number(stringQuantity);
+	if (matchingItem) {
+		matchingItem.quantity += quantity;
+	} else {
+		cart.push({
+			productId,
+			quantity,
+		});
+	}
+
+	saveToStorage();
+}
+
+export function removeFromCart(productId) {
+	const newCart = [];
+	cart.forEach((cartItem) => {
+		if (cartItem.productId !== productId) {
+			newCart.push(cartItem);
+		}
+	});
+	cart = newCart;
+	saveToStorage();
+}
