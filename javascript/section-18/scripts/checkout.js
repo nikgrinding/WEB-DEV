b80138b64d7f3a90ed39cbfdc339c6d0eb0e63fb
nikgrinding <nikgrinding@gmail.com>
2025-12-05 17:22:03 +0530

    05/12/2025

---
+++ javascript/section-18/scripts/checkout.js
@@ -0,0 +1,11 @@
+import { renderOrderSummary } from './checkout/orderSummary.js';
+import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
+import { loadProducts } from '../data/products.js';
+// import '../data/backend-practice.js';
+
+loadProducts(() => {
+	renderCheckoutHeader();
+	renderOrderSummary();
+	renderPaymentSummary();
+});
---
+++ javascript/section-18/tests-jasmine/checkout/orderSummaryTest.js
@@ -0,0 +1,104 @@
+import { renderOrderSummary } from '../../scripts/checkout/orderSummary.js';
+import { cart } from '../../data/cart-class.js';
+import { loadProducts } from '../../data/products.js';
+
+describe('test suite: renderOrderSummary', () => {
+	const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+	const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+	beforeAll((done) => {
+		loadProducts(() => {
+			done();
+		});
+	});
+
+	beforeEach(() => {
+		document.querySelector('.js-test-container').innerHTML = `
+            <div class="js-order-summary"></div>
+            <div class="js-checkout-header"></div>
+            <div class="js-payment-summary"></div>
+        `;
+		cart.cartItems = [
+			{
+				productId: productId1,
+				quantity: 2,
+				deliveryOptionId: '1',
+			},
+			{
+				productId: productId2,
+				quantity: 1,
+				deliveryOptionId: '2',
+			},
+		];
+
+		renderOrderSummary();
+	});
+
+	it('displays the cart', () => {
+		expect(
+			document.querySelectorAll('.js-cart-item-container').length
+		).toEqual(2);
+
+		expect(
+			document.querySelector(`.js-product-quantity-${productId1}`)
+				.innerText
+		).toContain('Quantity: 2');
+		expect(
+			document.querySelector(`.js-product-quantity-${productId2}`)
+				.innerText
+		).toContain('Quantity: 1');
+		expect(
+			document.querySelector(`.js-product-name-${productId1}`).innerText
+		).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
+		expect(
+			document.querySelector(`.js-product-price-${productId1}`).innerText
+		).toEqual(`$10.90`);
+		expect(
+			document.querySelector(`.js-product-price-${productId2}`).innerText
+		).toEqual(`$20.95`);
+	});
+
+	it('removes a product', () => {
+		document
+			.querySelector(`.js-delete-quantity-link-${productId1}`)
+			.click();
+
+		expect(
+			document.querySelectorAll('.js-cart-item-container').length
+		).toEqual(1);
+
+		expect(
+			document.querySelector(`.js-cart-item-container-${productId1}`)
+		).toEqual(null);
+		expect(
+			document.querySelector(`.js-cart-item-container-${productId2}`)
+		).not.toEqual(null);
+
+		expect(cart.cartItems.length).toEqual(1);
+		expect(cart.cartItems[0].productId).toEqual(productId2);
+	});
+
+	afterEach(() => {
+		document.querySelector('.js-test-container').innerHTML = '';
+	});
+
+	it('updates the delivery option', () => {
+		document.querySelector(`.js-delivery-option-${productId1}-3`).click();
+
+		expect(
+			document.querySelector(`.js-delivery-option-input-${productId1}-3`)
+				.checked
+		).toEqual(true);
+
+		expect(cart.cartItems.length).toEqual(2);
+		expect(cart.cartItems[0].productId).toEqual(productId1);
+		expect(cart.cartItems[0].deliveryOptionId).toEqual('3');
+
+		expect(
+			document.querySelector('.js-payment-summary-shipping').innerText
+		).toEqual('$14.98');
+		expect(
+			document.querySelector('.js-payment-summary-total').innerText
+		).toEqual('$63.50');
+	});
+});
