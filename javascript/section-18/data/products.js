365886204ea4c26549306ebc91e0e2b2ad344077
nikgrinding <nikgrinding@gmail.com>
2025-12-08 19:15:40 +0530

    08/12/2025

--- javascript/section-18/data/products.js
+++ javascript/section-18/data/products.js
@@ -585,6 +585,32 @@ export class Appliance extends Product {
 
 export let products = [];
 
+export function loadProductsFetch() {
+	const promise = fetch('https://supersimplebackend.dev/products')
+		.then((response) => {
+			return response.json();
+		})
+		.then((productsData) => {
+			products = productsData.map((productDetails) => {
+				if (productDetails.type === 'clothing') {
+					return new Clothing(productDetails);
+				} else if (productDetails.type === 'appliance') {
+					return new Appliance(productDetails);
+				}
+				return new Product(productDetails);
+			});
+			console.log('load products');
+		})
+		.catch((error) => {
+			console.log('unexpected error. please try again later');
+		});
+	return promise;
+}
+
+// loadProductsFetch().then(() => {
+// 	console.log('next step');
+// });
+
 export function loadProducts(fun) {
 	const xhr = new XMLHttpRequest();
 	xhr.addEventListener('load', () => {
@@ -598,6 +624,9 @@ export function loadProducts(fun) {
 		});
 		console.log('load products');
 		fun();
+	});
+	xhr.addEventListener('error', (error) => {
+		console.log('unexpected error. please try again later');
 	});
 	xhr.open('GET', 'https://supersimplebackend.dev/products');
 	xhr.send();
--- javascript/section-18/scripts/checkout.js
+++ javascript/section-18/scripts/checkout.js
@@ -1,11 +1,64 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
-import { loadProducts } from '../data/products.js';
-// import '../data/backend-practice.js';
+import { loadProductsFetch } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-loadProducts(() => {
+async function loadPage() {
+	try {
+		await loadProductsFetch();
+		await new Promise((resolve, reject) => {
+			loadCart(() => {
+				resolve();
+				// reject('error3');
+			});
+		});
+	} catch (error) {
+		console.log('unexpected error. please try again later');
+	}
 	renderCheckoutHeader();
 	renderOrderSummary();
 	renderPaymentSummary();
-});
+}
+loadPage();
+
+// Promise.all([
+// 	loadProductsFetch(),
+// new Promise((resolve) => {
+// 	loadCart(() => {
+// 		resolve();
+// 	});
+// }),
+// ]).then((values) => {
+// 	console.log(values);
+// renderCheckoutHeader();
+// renderOrderSummary();
+// renderPaymentSummary();
+// });
+
+// new Promise((resolve) => {
+// 	loadProducts(() => {
+// 		resolve('value1');
+// 	});
+// })
+// 	.then((value) => {
+// 		console.log(value);
+// 		return new Promise((resolve) => {
+// 			loadCart(() => {
+// 				resolve();
+// 			});
+// 		});
+// 	})
+// 	.then(() => {
+// 		renderCheckoutHeader();
+// 		renderOrderSummary();
+// 		renderPaymentSummary();
+// 	});
+
+// loadProducts(() => {
+// 	loadCart(() => {
+// 		renderCheckoutHeader();
+// 		renderOrderSummary();
+// 		renderPaymentSummary();
+// 	});
+// });
