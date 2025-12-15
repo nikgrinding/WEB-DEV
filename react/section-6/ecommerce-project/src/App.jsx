5ec34ae0f5bc427a663c0a2868a9a56692e02572
nikgrinding <nikgrinding@gmail.com>
2025-12-15 15:30:47 +0530

    move tracking page to react

--- react/section-6/ecommerce-project/src/App.jsx
+++ react/section-6/ecommerce-project/src/App.jsx
@@ -1,8 +1,9 @@
+import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import CheckoutPage from "./pages/CheckoutPage";
 import OrdersPage from "./pages/OrdersPage";
+import TrackingPage from "./pages/TrackingPage";
 import "./App.css";
-import { Routes, Route } from "react-router";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route index element={<HomePage />} />
                 <Route path="/checkout" element={<CheckoutPage />} />
                 <Route path="/orders" element={<OrdersPage />} />
+                <Route path="/tracking" element={<TrackingPage />} />
             </Routes>
         </>
     );
