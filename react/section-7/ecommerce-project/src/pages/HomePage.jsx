9ff320129c7714e7f1ddbddf32edce3aa313d89d
nikgrinding <nikgrinding@gmail.com>
2025-12-16 13:35:01 +0530

    use data from backend to generate the HTML

--- react/section-7/ecommerce-project/src/pages/HomePage.jsx
+++ react/section-7/ecommerce-project/src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
 import "./HomePage.css";
 import Header from "../components/Header";
 import Checkmark from "../assets/images/icons/checkmark.png";
-import { products } from "../../starting-code/data/products.js";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
-    axios.get("http://localhost:3000/api/products").then((response) => {
-        console.log(response.data);
-    });
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/products").then((response) => {
+            setProducts(response.data);
+        });
+    }, []);
 
     return (
         <>
