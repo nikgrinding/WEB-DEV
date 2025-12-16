8d95c18d0f127ab7a02b9a3a96479054cacfa157
nikgrinding <nikgrinding@gmail.com>
2025-12-16 13:19:31 +0530

    fetch data from backend

--- react/section-7/ecommerce-project/src/pages/HomePage.jsx
+++ react/section-7/ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,14 @@ import Checkmark from "../assets/images/icons/checkmark.png";
 import { products } from "../../starting-code/data/products.js";
 
 export default function HomePage() {
+    fetch("http://localhost:3000/api/products")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+        });
+
     return (
         <>
             <link rel="icon" href="home-favicon.png"></link>
