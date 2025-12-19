77df85f76fde63e7c5739f90d3b020dafd9523f3
nikgrinding <nikgrinding@gmail.com>
2025-12-19 18:24:50 +0530

    payment summary variable fix

--- react/section-8/ecommerce-project/src/pages/checkout/PaymentSummary.jsx
+++ react/section-8/ecommerce-project/src/pages/checkout/PaymentSummary.jsx
@@ -10,27 +10,24 @@ export default function PaymentSummary({ paymentSummary }) {
                         <div>Items ({paymentSummary.totalItems}):</div>
                         <div className="payment-summary-money">{formatMoney(paymentSummary.productCostCents)}</div>
                     </div>
-
                     <div className="payment-summary-row">
                         <div>Shipping &amp; handling:</div>
                         <div className="payment-summary-money">{formatMoney(paymentSummary.shippingCostCents)}</div>
                     </div>
-
                     <div className="payment-summary-row subtotal-row">
                         <div>Total before tax:</div>
-                        <div className="payment-summary-money">{formatMoney(paymentSummary.totalBeforeTaxCents)}</div>
+                        <div className="payment-summary-money">
+                            {formatMoney(paymentSummary.totalCostBeforeTaxCents)}
+                        </div>
                     </div>
-
                     <div className="payment-summary-row">
                         <div>Estimated tax (10%):</div>
                         <div className="payment-summary-money">{formatMoney(paymentSummary.taxCents)}</div>
                     </div>
-
                     <div className="payment-summary-row total-row">
                         <div>Order total:</div>
-                        <div className="payment-summary-money">{formatMoney(paymentSummary.totalCents)}</div>
+                        <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostCents)}</div>
                     </div>
-
                     <button className="place-order-button button-primary">Place your order</button>
                 </>
             )}
