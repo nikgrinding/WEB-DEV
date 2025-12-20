1863322bfc9b2c5d3b300928e8836ff873061dcc
nikgrinding <nikgrinding@gmail.com>
2025-12-20 11:53:00 +0530

    http module

---
+++ node-express-mongodb/12-http.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.end("welcome to our home page");
+    } else if (req.url === "/about") {
+        res.end("our short history");
+    } else {
+        res.end(`<h1>Oops</h1>
+			<p>we can't find this page</p>
+			<a href="/">click here to go back home</a>`);
+    }
+});
+
+server.listen(5000);
