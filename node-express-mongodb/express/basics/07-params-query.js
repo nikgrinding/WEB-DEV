a9ac9498f4ef1cea8d5be506ffc4238337606039
nikgrinding <nikgrinding@gmail.com>
2025-12-23 06:01:12 +0530

    serve response based on query and params

---
+++ node-express-mongodb/express/basics/07-params-query.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const { products } = require("./data.js");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send('<h1>Home Page</h1><a href = "/api/products">Products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product;
+        return { id, name, image };
+    });
+    res.json(newProducts);
+});
+
+app.get("/api/products/:productId", (req, res) => {
+    const { productId } = req.params;
+    const singleProduct = products.find((product) => {
+        if (product.id === Number(productId)) {
+            return product;
+        }
+    });
+    if (!singleProduct) {
+        res.status(404).send("Product does not exist");
+    }
+    res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+    console.log(req.params);
+    res.send("hello world");
+});
+
+app.get("/api/v1/query", (req, res) => {
+    const { search, limit } = req.query;
+    let sortedProducts = [...products];
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search);
+        });
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+    if (sortedProducts.length === 0) {
+        return res.status(200).send("no products matched");
+    }
+    return res.status(200).send(sortedProducts);
+});
+
+app.listen(5000, () => {
+    console.log("server is listening at 5000");
+});
