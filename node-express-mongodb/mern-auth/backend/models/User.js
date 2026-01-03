bf8c2581f2204edbc2a93a42cd83505e76ae67db
nikgrinding <nikgrinding@gmail.com>
2026-01-03 21:56:17 +0530

    Add email format and password length validation to User model

--- node-express-mongodb/mern-auth/backend/models/User.js
+++ node-express-mongodb/mern-auth/backend/models/User.js
@@ -13,10 +13,13 @@ const userSchema = new mongoose.Schema(
             unique: true,
             lowercase: true,
             trim: true,
+            match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
         },
         password: {
             type: String,
             required: [true, "Password is required."],
+            minlength: [6, "Password must be at least 6 characters long"],
+            select: false,
         },
         verifyOTP: {
             type: String,
