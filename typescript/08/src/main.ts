bd16db08ae5b9ffc79fd945e4c51b8579eb9621e
nikgrinding <nikgrinding@gmail.com>
2026-01-19 21:45:07 +0530

    more generics

--- typescript/08/src/main.ts
+++ typescript/08/src/main.ts
@@ -120,3 +120,28 @@ const usersArray = [
 
 console.log(getUsersProperty(usersArray, "email"));
 console.log(getUsersProperty(usersArray, "username"));
+
+class StateObject<T> {
+    private data: T;
+
+    constructor(value: T) {
+        this.data = value;
+    }
+
+    get state(): T {
+        return this.data;
+    }
+
+    set state(value: T) {
+        this.data = value;
+    }
+}
+
+const store = new StateObject("Nik");
+console.log(store.state);
+store.state = "Nick";
+//store.state = 12
+
+const myState = new StateObject<(string | number | boolean)[]>([15]);
+myState.state = ["Nik", 20, true];
+console.log(myState.state);
