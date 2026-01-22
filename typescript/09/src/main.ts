be45baf9b548249dc02256b0ae01292accd2c5d1
nikgrinding <nikgrinding@gmail.com>
2026-01-22 23:31:46 +0530

    more utility types

--- typescript/09/src/main.ts
+++ typescript/09/src/main.ts
@@ -91,3 +91,25 @@ const assignArgs: AssignParams = ["Generics", 100];
 
 const tsAssign2: NewAssign = createNewAssign(...assignArgs);
 console.log(tsAssign2);
+
+interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+}
+
+const fetchUsers = async (): Promise<User[]> => {
+    const data = await fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => {
+            return res.json();
+        })
+        .catch((err) => {
+            if (err instanceof Error) console.log(err.message);
+        });
+    return data;
+};
+
+type FetchUsersReturnType = Awaited<ReturnType<typeof fetchUsers>>;
+
+fetchUsers().then((users) => console.log(users));
